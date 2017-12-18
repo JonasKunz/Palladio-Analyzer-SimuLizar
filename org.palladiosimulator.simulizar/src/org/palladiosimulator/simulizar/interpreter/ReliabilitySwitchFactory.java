fc6a7f94af84f7979fe58d0cde3f8ba9d8ab694f
JonasKunz <Jonas.Kunz@gmx.net>
2017-12-18 11:59:21 +0100

    Added Failure Events to listener.

--- org.palladiosimulator.simulizar/src/org/palladiosimulator/simulizar/interpreter/ReliabilitySwitchFactory.java
+++ org.palladiosimulator.simulizar/src/org/palladiosimulator/simulizar/interpreter/ReliabilitySwitchFactory.java
@@ -1,13 +1,12 @@
 package org.palladiosimulator.simulizar.interpreter;
 
-import org.eclipse.emf.ecore.util.ComposedSwitch;
 import org.eclipse.emf.ecore.util.Switch;
 import org.palladiosimulator.simulizar.runtimestate.SimulatedBasicComponentInstance;
 
 public class ReliabilitySwitchFactory extends AbstractRDSeffSwitchFactory{
 
 	@Override
-	protected Switch<Object> createRDSeffSwitch(InterpreterDefaultContext context, SimulatedBasicComponentInstance basicComponentInstance, ComposedSwitch<Object> parentSwitch) {
+	protected Switch<Object> createRDSeffSwitch(InterpreterDefaultContext context, SimulatedBasicComponentInstance basicComponentInstance, ExplicitDispatchComposedSwitch<Object> parentSwitch) {
 		return new ReliabilitySwitch(context, parentSwitch);
 	}
 
